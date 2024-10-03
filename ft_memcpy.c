d3e8dd3228a8cb6f1687b30aa178ae6b81d7d3e6
Don Lazzylife <88842775+lazzylife42@users.noreply.github.com>
2024-10-03 03:01:41 +0200

    askipjarrive

---
+++ ft_memcpy.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+
+void * ft_memcpy(void *dst, const void *src, size_t)
+{
+	unsigned int *d;
+	unsigned int *s;
+	d = dst;
+	s = src;
+	
+	if (d == NULL && s == NULL)
+		return(d);
+
+		
+
+
+}
